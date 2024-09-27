6f25101972b44a9762fe2743814f73df5b4c6826
Zachary LaCore <zlacore12@gmail.com>
2024-09-26 19:15:25 -0500

    addedjsfile

--- assets/scripts/profile.js
+++ assets/scripts/profile.js
@@ -1,1 +1,18 @@
 const myDoc = document;
+const blimpBtn = document.getElementById('postPet');
+
+//TODO: Add functionality to postPets buton.
+//Button function should: 
+//Remove h3 from DOM
+//Remove post button from DOM
+//Create and append title Input to DOM with custom ID
+//Create and append textbox to DOM with custom ID
+//Create and append image input box to DOM with specific ID
+//Create and append submitblog button to DOM
+
+blimpBtn.addEventListener('click', function () {
+const npb = myDoc.getElementById("noPets");
+const pP = myDoc.getElementById("postPet");
+const petImgDiv = myDoc.createElement("div");
+const petImgInp = myDoc.createElement()
+});
