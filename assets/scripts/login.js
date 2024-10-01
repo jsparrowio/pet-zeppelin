5a69309921e433afef105bfc369e4b744ec16b12
jsparrowio <jsparrowio@outlook.com>
2024-09-30 19:31:30 -0700

    added code to global.js and login.js

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,4 +1,3 @@
-const myDoc = document;
 const loginbutton = myDoc.getElementById('loginSubmit');
 const usernameEl = myDoc.getElementById('loginUsername');
 const passwordEl = myDoc.getElementById('loginPassword');
@@ -9,32 +8,56 @@ loginbutton.addEventListener('click', function (event) {
     event.preventDefault();
     console.log("DING");
     //TODO IF LocalStorage doesn't have an object that contains a matching username
-    const fetchProfile = JSON.parse(localStorage.getItem(usernameEl.value));
-    if(fetchProfile===null){
-        //Create a new profile
-        newProf = newProfile();
-        localStorage.setItem(usernameEl.value,JSON.stringify(newProf));
-        localStorage.setItem('currentUser',JSON.stringify(newProf));
-        //GO to main page maybe make this current profile?
-    }
-    else{
+    if (users.length > 0 && users) {
+    let i = 0;
+    for (i = 0; i < users.length; i++) {
+    if(users[i].username.includes(usernameEl.value)){
         //check to see if password matches
-        if(fetchProfile.password === passwordEl.value){
-            localStorage.setItem('currentUser',JSON.stringify(fetchProfile));
+        if(users[i].password === passwordEl.value){
+            const currentUser = {
+                username: usernameEl.value,
+                password: passwordEl.value
+            }
+            localStorage.setItem('currentUser',JSON.stringify(currentUser));
+            alertSuccess('Login successfull!');
+            return;
             //LOGIN SUCCESS <-- what does this mean? Current Profile = this profile object?
             //GO to main page
         }
-        else{
-            alert("wrong password stupid")
+        else {
+            alertWarning('Wrong password stupid');
             //change paragraph element in login to say 'invalid username or password'
+            return;
         }
     }
+  }
+if(i === users.length){
+    //Create a new profile
+    newProfile();
+    alertSuccess('New user registered!');
+    //GO to main page maybe make this current profile?
+    return;
+}
+}  else {
+    console.log('New User Registered!');
+    //Create a new profile
+    newProfile();
+    alertSuccess('New user registered!');
+    return;
+    //GO to main page maybe make this current profile?
+}
 });
 
 function newProfile() {
-    const profile = {
+    users.push({
+        username: usernameEl.value,
+        password: passwordEl.value
+    })
+    const currentUser = {
         username: usernameEl.value,
         password: passwordEl.value
     }
-    return profile;
+    localStorage.setItem('currentUser',JSON.stringify(currentUser));
+    storeLocalStorage();
+    return users;
 }
