0bc30c18d464ad64517eaafd7fa1d30da9b8dbc6
Matt Oberdalhoff <reliable.vagabond@gmail.com>
2024-10-01 19:37:18 -0500

    refined the code so all it needs is gets and sets for post objects

--- assets/scripts/blog.js
+++ assets/scripts/blog.js
@@ -1,4 +1,5 @@
-const mainEl = myDoc.querySelector('main');
+const postsEl = myDoc.getElementById('posts');
+const submitButton = myDoc.getElementById('postSubmit');
 
 function renderPosts(){
     // TODO: Clear all elements attached to main?
@@ -7,10 +8,9 @@ function renderPosts(){
         buildPost(postObj);
     }
 }
-// TODO: Add a fuction for making a post, that add the post parameters to an object, adds that to the post array, 
-//      then rerenders the posts to add the most recent post
-//      parameters: image input, post title, post content
+
 function submitPost(){
+    let post = [];
     // create a new post by populating an object with parameters
     const imgSubmit = myDoc.getElementById('postImage');
     const titleSubmit = myDoc.getElementById('postTitle');
@@ -21,7 +21,7 @@ function submitPost(){
         username: currentUser.username,
         title: titleSubmit, 
         content: contentSubmit 
-    }
+    };
     post = ''; //TODO Add readStorage fuction to add parsed array of post objects
     post.push(newPost);
     // TODO add storeLocalStorage function here
@@ -42,11 +42,17 @@ function buildPost(singleObject){
     contentEl.textContent = singleObject.content;
     //append everything together
     articleEl.appendChild(imgEl);
-    articleEl.appendChild(imgEl);
-    articleEl.appendChild(imgEl);
-    articleEl.appendChild(imgEl);
-    //TODO use .insertBefore to add element group to main
+    articleEl.appendChild(userEl);
+    articleEl.appendChild(titleEl);
+    articleEl.appendChild(contentEl);
+    //check if the posts div is empty, if not, then put the next added post above the first post
+    if(postsEl.hasChildNodes()){
+        postsEl.insertBefore(articleEl,postsEl.firstChild);
+    } else {
+        postsEl.appendChild(articleEl);
+    }
 
 }
 
+submitButton.addEventListener('click', submitPost());
 //TODO add event listner for submit button on post
